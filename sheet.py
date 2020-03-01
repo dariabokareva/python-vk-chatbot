f2100028093448efec0c6904f5977a6b3fef7b67
Daria Bokareva <52369969+dariabokareva@users.noreply.github.com>
2020-03-01 20:39:24 +0300

    Update sheet.py

--- sheet.py
+++ sheet.py
@@ -1,27 +1,20 @@
 from typing import List, Any
 import httplib2  # google-api-python-client
-from httplib2 import ProxyInfo, socks
 from apiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials  # oauth2client
 import pandas as pd
-from urllib.parse import urlparse
-import os
 import numpy as np
 
 # from server import Server
 
-CREDENTIALS_FILE = 'chatbot-vk-instagram-11994c762d7a.json'
-SPREADSHEET_ID = '1ZC8ll2CE9KKEU8JdXm2nJmusivOlTiLLN3Hka9Y82S0'
-RANGE_NAME = 'B4:F500'
+CREDENTIALS_FILE = 'your-google-credentials-file.json'
+SPREADSHEET_ID = 'your google shet id'
+RANGE_NAME = 'cell range (ex. A1:E5)'
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
 key = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
 
-# proxy_netloc = urlparse(os.environ.get("http_proxy")).netloc.split(':')
-'''httpAuth = key.authorize(httplib2.Http(proxy_info=ProxyInfo(proxy_type=socks.PROXY_TYPE_HTTP,
-                                                            proxy_host=proxy_netloc[0],
-                                                            proxy_port=int(proxy_netloc[1]))))'''
 httpAuth = key.authorize(httplib2.Http())
 print(httpAuth)
 print(key)
